82a28f87da8ceb0617db606f0c2812a80495c095
Owen-Spooner <spoo6249@bears.unco.edu>
2023-03-07 12:52:11 -0700

    User Story Code
    Code for 3 User stories (unfinished)

---
+++ userStory(Sort).py
@@ -0,0 +1,15 @@
+
+
+index = 0
+region = ""
+industry = ""
+
+company1 = [index, region, industry]
+company2 = [index, region, industry]
+company3 = [index, region, industry]
+
+companies = [company1, company2, company3]
+
+print(companies[0])
+#fill in for api
+
