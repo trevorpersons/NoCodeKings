1855e2033fbfce4f719d06130f1aeb04e9332dbe
Owen-Spooner <spoo6249@bears.unco.edu>
2023-04-05 14:12:38 -0600

    Update

--- StockPriceW/CompanyList.py
+++ StockPriceW/CompanyList.py
@@ -11,15 +11,16 @@ response = requests.get(url)
 if response.status_code == 200:
     # Parse the JSON response and extract the data dictionary
     data = response.json()
-
-    # Print the data dictionary and its keys
+    index = 12
+    print(len(data))
     
-    index = 12345
-
+    
     url1 = "https://financialmodelingprep.com/api/v3/quote/"+data[index]+",FB?apikey=d2b1cf9beb66264ece3054788678d1b4"
     response1 = requests.get(url1)
     data1 = response1.json()
     print(data1)
+
+
     
     
     
