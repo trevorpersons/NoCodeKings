1218e39caf6667d015100e7d1ad9e506fe8c4d40
trevorpersons <63942317+trevorpersons@users.noreply.github.com>
2023-04-05 20:07:04 -0600

    financials application stock function pt 1

--- financials/urls.py
+++ financials/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='financials_index')
+    path('', views.index, name='financials_index'),
+
+    path('stock_info/', views.stock_info, name='stock_info'),
 ]
+
--- financials/views.py
+++ financials/views.py
@@ -1,8 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import requests
 
 # Create your views here.
-
-
 def index(request):
-    return HttpResponse("Financial Page")
+    return HttpResponse("Hello")
+
+def stock_info(request):
+    api_key = 'd2b1cf9beb66264ece3054788678d1b4'
+    symbol = 'AAPL'  # Replace with the stock symbol of your choice
+
+    url = f'https://financialmodelingprep.com/api/v3/quote/{symbol}?apikey={api_key}'
+    response = requests.get(url)
+    stock_data = response.json()
+
+    context = {
+        'symbol': stock_data[0]['symbol'],
+        'name': stock_data[0]['name'],
+        'price': stock_data[0]['price'],
+        'change': stock_data[0]['change'],
+        'change_pct': stock_data[0]['changesPercentage'],
+    }
+
+    return render(request, 'stock_info.html', context)
+
