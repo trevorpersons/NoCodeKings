6e3be1c7ae24fd235570e3acb7034b7a633ab184
trevorpersons <63942317+trevorpersons@users.noreply.github.com>
2023-04-05 20:42:11 -0600

    dynamic search function

--- financials/urls.py
+++ financials/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='financials_index'),
-
     path('stock_info/', views.stock_info, name='stock_info'),
+    path('stock_info/<str:symbol>/', views.stock_info, name='stock_info_symbol'),
 
 ]
 
--- financials/views.py
+++ financials/views.py
@@ -3,17 +3,13 @@ from django.shortcuts import render
 import requests
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello")
-
-def stock_info(request):
+def stock_info(request, symbol=None):
     api_key = 'd2b1cf9beb66264ece3054788678d1b4'
-    symbol = 'AAPL'  # Replace with the stock symbol of your choice
-
+    if symbol is None:
+        symbol = 'AAPL'
     url = f'https://financialmodelingprep.com/api/v3/quote/{symbol}?apikey={api_key}'
     response = requests.get(url)
     stock_data = response.json()
-
     context = {
         'symbol': stock_data[0]['symbol'],
         'name': stock_data[0]['name'],
@@ -21,6 +17,5 @@ def stock_info(request):
         'change': stock_data[0]['change'],
         'change_pct': stock_data[0]['changesPercentage'],
     }
-
     return render(request, 'stock_info.html', context)
 
