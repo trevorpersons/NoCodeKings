0ef822a1b84620778ac960c5d29a50590d61e129
trevorpersons <63942317+trevorpersons@users.noreply.github.com>
2023-04-06 23:09:30 -0600

    changes to urls and stock info

--- financials/urls.py
+++ financials/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.index, name='financials_index'),
 
     path('stock_info/', views.stock_info, name='stock_info'),
-    
+
     path('stock_info/<str:symbol>/', views.stock_info, name='stock_info_symbol'),
 
 ]
