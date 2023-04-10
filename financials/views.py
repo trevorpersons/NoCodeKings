943f3e7217ea7b6bc51878a9a6d19fed0c207152
trevorpersons <63942317+trevorpersons@users.noreply.github.com>
2023-04-10 16:05:12 -0600

    working code changes

--- financials/views.py
+++ financials/views.py
@@ -1,18 +1,23 @@
 from django.http import HttpResponse
-from django.shortcuts import render 
+from django.shortcuts import render
 import requests
 
 # Create your views here.
 def index(request):
     return render(request, 'index2.html')
 
-def stock_info(request, symbol=None):
-    
-    if symbol is None:
+def stock_info(request):
+    api_key = 'd2b1cf9beb66264ece3054788678d1b4'
+
+    if request.method == 'POST':
+        symbol = request.POST.get('symbol', 'AAPL')
+    else:
         symbol = 'AAPL'
-    url = f'https://financialmodelingprep.com/api/v3/quote/{symbol}?apikey=d2b1cf9beb66264ece3054788678d1b4'
+
+    url = f'https://financialmodelingprep.com/api/v3/quote/{symbol}?apikey={api_key}'
     response = requests.get(url)
     stock_data = response.json()
+
     context = {
         'symbol': stock_data[0]['symbol'],
         'name': stock_data[0]['name'],
@@ -20,5 +25,6 @@ def stock_info(request, symbol=None):
         'change': stock_data[0]['change'],
         'change_pct': stock_data[0]['changesPercentage'],
     }
+
     return render(request, 'stock_info.html', context)
 
