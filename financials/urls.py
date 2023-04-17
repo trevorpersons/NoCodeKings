3c2f80916715263aeabcad96a1cc5b141a38f6e6
Andrew-Grove1 <grov6230@bears.unco.edu>
2023-04-17 17:50:32 -0600

    trying to get my server to run... didn't work

--- financials/urls.py
+++ financials/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
     path('', views.index, name='financials_index'),
     path('stock_info/', views.stock_info, name='stock_info'),
     path('stock_info/<str:symbol>/', views.stock_info, name='stock_info_symbol'),
